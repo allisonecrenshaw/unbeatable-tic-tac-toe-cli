18d81c1d6c0b620a7f242602f6c56d1b3a2b6788
Allison Crenshaw <allison.crenshaw@atmosphere.tv>
2023-10-12 14:55:39 -0500

    Added initializeBoard and displayBoard function with some temp filler code -- just getting basic structures started.

--- board.js
+++ board.js
@@ -1,4 +1,18 @@
 // Declaration
-class Board {
-    constructor() {}
+export class Board {
+	initializeBoard() {
+		this.cells = [
+			[null, null, null],
+			[null, null, null],
+			[null, null, null],
+		];
+	}
+
+	displayBoard() {
+		console.log(" ___________ ");
+		console.log("| X | X |   |");
+		console.log("| O | X | O |");
+		console.log("| O | X | O |");
+		console.log(" ___________ ");
+	}
 }
