9bd4899f83e99e25722102014e99d896159d5d0c
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-10-12 17:21:28 -0500

    Working on displayBoard. Still in progress.

--- board.js
+++ board.js
@@ -2,17 +2,26 @@
 export class Board {
 	initializeBoard() {
 		this.cells = [
-			[null, null, null],
-			[null, null, null],
-			[null, null, null],
+			// [null, null, null],
+			// [null, null, null],
+			// [null, null, null],
+			["X", "X", "X"],
+			["X", "X", "X"],
+			["X", "X", "X"],
 		];
 	}
 
 	displayBoard() {
-		console.log(" ___________ ");
-		console.log("| X | X |   |");
-		console.log("| O | X | O |");
-		console.log("| O | X | O |");
-		console.log(" ___________ ");
+		for (var rowIndex = 0; rowIndex < this.cells.length; rowIndex++) {
+			for (var columnIndex = 0; columnIndex < this.cells[rowIndex].length; columnIndex++) {
+				if (columnIndex < this.cells[rowIndex].length - 1) {
+					// console.log("| ", this.cells[rowIndex[columnIndex]]);
+					console.log("| ", "X");
+				} else {
+					// console.log("| ", this.cells[rowIndex[columnIndex]], +" |");
+					console.log("| ", "X", " |");
+				}
+			}
+		}
 	}
 }
