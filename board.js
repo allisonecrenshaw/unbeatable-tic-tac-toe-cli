1f087ead112ec78d03a7e26e67ebf57fe6e47cde
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-10-13 16:30:46 -0500

    Improved displayBoard.

--- board.js
+++ board.js
@@ -2,26 +2,22 @@
 export class Board {
 	initializeBoard() {
 		this.cells = [
-			// [null, null, null],
-			// [null, null, null],
-			// [null, null, null],
-			["X", "X", "X"],
-			["X", "X", "X"],
-			["X", "X", "X"],
+			[" ", " ", " "],
+			[" ", " ", " "],
+			[" ", " ", " "],
+			// ["X", "X", "X"],
+			// ["X", "X", "X"],
+			// ["X", "X", "X"],
 		];
 	}
 
 	displayBoard() {
+		var line = "-------------";
+		console.log(line);
 		for (var rowIndex = 0; rowIndex < this.cells.length; rowIndex++) {
-			for (var columnIndex = 0; columnIndex < this.cells[rowIndex].length; columnIndex++) {
-				if (columnIndex < this.cells[rowIndex].length - 1) {
-					// console.log("| ", this.cells[rowIndex[columnIndex]]);
-					console.log("| ", "X");
-				} else {
-					// console.log("| ", this.cells[rowIndex[columnIndex]], +" |");
-					console.log("| ", "X", " |");
-				}
-			}
+			var thisRow = this.cells[rowIndex];
+			console.log("|", thisRow[0], "|", thisRow[1], "|", thisRow[2], "|");
 		}
+		console.log(line);
 	}
 }
