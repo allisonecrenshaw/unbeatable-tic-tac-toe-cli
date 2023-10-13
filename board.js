7af3d184e207efe67b62b821345b70b2c9e385a1
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-10-13 16:34:43 -0500

    Added column and row labels to displayBoard.

--- board.js
+++ board.js
@@ -12,11 +12,14 @@ export class Board {
 	}
 
 	displayBoard() {
-		var line = "-------------";
+		var line = "  -------------";
+		var columnLabels = "    A   B   C";
+		console.log();
+		console.log(columnLabels);
 		console.log(line);
 		for (var rowIndex = 0; rowIndex < this.cells.length; rowIndex++) {
 			var thisRow = this.cells[rowIndex];
-			console.log("|", thisRow[0], "|", thisRow[1], "|", thisRow[2], "|");
+			console.log(rowIndex + 1, "|", thisRow[0], "|", thisRow[1], "|", thisRow[2], "|");
 		}
 		console.log(line);
 	}
