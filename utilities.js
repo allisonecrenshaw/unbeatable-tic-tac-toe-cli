e73198e0a99c9b94ee7a1b1d2a40d2cce4b3691f
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-10-16 12:55:37 -0500

    Fixed typo in welcome message.

--- utilities.js
+++ utilities.js
@@ -1,36 +1,36 @@
 import * as readlineSync from "readline-sync";
 
 export function displayWelcomeAndGetGameMode() {
-    displayWelcomeMessage();
+	displayWelcomeMessage();
 
-    displayGameModeMessage();
+	displayGameModeMessage();
 
-    var gameMode = getGameMode();
+	var gameMode = getGameMode();
 
-    displayGameModeConfirmation(gameMode);
+	displayGameModeConfirmation(gameMode);
 
-    return gameMode;
+	return gameMode;
 }
 
 export function displayWelcomeMessage() {
-    var line = "**************************";
-    console.log();
-    console.log(line);
-    console.log("Welcomem to Tic Taco Toe!");
-    console.log(line);
+	var line = "**************************";
+	console.log();
+	console.log(line);
+	console.log("Welcomem to Tic Tac Toe!");
+	console.log(line);
 }
 
 export function displayGameModeMessage() {
-    console.log("\nYou can play against the computer or against a friend.");
+	console.log("\nYou can play against the computer or against a friend.");
 }
 
 export function getGameMode() {
-    const gameMode = readlineSync.question(
-        "\nPlease enter your preferred game mode (type 'computer' or 'friend'): "
-    );
-    return gameMode;
+	const gameMode = readlineSync.question(
+		"\nPlease enter your preferred game mode (type 'computer' or 'friend'): "
+	);
+	return gameMode;
 }
 
 export function displayGameModeConfirmation(gameMode) {
-    console.log("\nYou have chosen to play in ", gameMode, " mode.");
+	console.log("\nYou have chosen to play in ", gameMode, " mode.");
 }
