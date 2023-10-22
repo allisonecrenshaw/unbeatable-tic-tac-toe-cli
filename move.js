1e9b2bf7e5f7fd85d957b245f511af4ae5dffd41
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-10-22 11:36:31 -0500

    Starting column validation. Next, need to remove validation functions from board and call directly from move's functions.

--- move.js
+++ move.js
@@ -1,43 +1,45 @@
 export class Move {
-	constructor(player, coordinates) {
-		const { isValid, row, column } = this.validateCoordinate(coordinates);
+    constructor(player, coordinates) {
+        const { isValid, row, column } = this.validateCoordinate(coordinates);
 
-		if (!isValid) {
-			throw new Error("Invalid coordinates.");
-		}
+        if (!isValid) {
+            throw new Error("Invalid coordinates.");
+        }
 
-		this.player = player;
-		this.row = row;
-		this.column = column;
-	}
+        this.player = player;
+        this.row = row;
+        this.column = column;
+    }
 
-	isMoveValid(coordinates) {
-		if (coordinates.length != 2) {
-			return false;
-		}
+    isMoveValid(coordinates) {
+        if (coordinates.length != 2) {
+            return false;
+        }
 
-		const row = coordinates.charAt(0);
-		const column = coordinates.charAt(1);
+        const row = coordinates.charAt(0);
+        const column = coordinates.charAt(1);
 
-		const isValidRow = this.isRowValid(row);
-		const isValidColumn = this.isColumnValid(column);
+        const isValidRow = this.isRowValid(row);
+        const isValidColumn = this.isColumnValid(column);
 
-		return {
-			isValid: isValidRow && isValidColumn,
-			row,
-			column,
-		};
-	}
+        return {
+            isValid: isValidRow && isValidColumn,
+            row,
+            column,
+        };
+    }
 
-	isRowValid(row) {
-		if (row == "A" || row == "B" || row == "C") {
-			return true;
-		}
-		return false;
-	}
+    isRowValid(row) {
+        if (row == "A" || row == "B" || row == "C") {
+            return true;
+        }
+        return false;
+    }
 
-	isColumnValid(column) {
-		// Implement column validation logic
-		// Return true if valid, false otherwise
-	}
+    isColumnValid(column) {
+        if (column == "1" || column == "2" || column == "3") {
+            return true;
+        }
+        return false;
+    }
 }
