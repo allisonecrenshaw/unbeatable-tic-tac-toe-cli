2e48b17a7bb2dc82af65079012d093020b47ddc5
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-10-27 17:14:33 -0500

    Added functions to transform coordinates into index for later updating board.

--- move.js
+++ move.js
@@ -3,4 +3,28 @@ export class Move {
 		this.player = player;
 		this.coordinate = coordinate;
 	}
+
+	transformXCoordinateIntoIndex(coordinate) {
+		if (coordinate == "A") {
+			return 0;
+		} else if (coordinate == "B") {
+			return 1;
+		} else if (coordinate == "C") {
+			return 2;
+		} else {
+			throw new Error("Invalid coordinate: x value invalid.");
+		}
+	}
+
+	transformYCoordinateIntoIndex(coordinate) {
+		if (coordinate == "1") {
+			return 0;
+		} else if (coordinate == "2") {
+			return 1;
+		} else if (coordinate == "3") {
+			return 2;
+		} else {
+			throw new Error("Invalid coordinate: y value invalid.");
+		}
+	}
 }
