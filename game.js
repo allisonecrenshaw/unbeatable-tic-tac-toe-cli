8973c27794145e57a1b4b1a4b395595854429b10
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-10-28 09:33:44 -0500

    Change name of turn function.

--- game.js
+++ game.js
@@ -4,37 +4,37 @@ import { Move } from "./move.js";
 import * as readlineSync from "readline-sync";
 
 export class Game {
-	constructor(gameMode, player1, player2) {
-		this.board = new Board();
-		this.player1 = player1;
-		this.player2 = player2;
-		this.currentPlayer = player1;
-		this.gameMode = gameMode;
-		this.gameWon = false;
-		this.winner = null;
-	}
+    constructor(gameMode, player1, player2) {
+        this.board = new Board();
+        this.player1 = player1;
+        this.player2 = player2;
+        this.currentPlayer = player1;
+        this.gameMode = gameMode;
+        this.gameWon = false;
+        this.winner = null;
+    }
 
-	play() {
-		console.log("\nLet's play!");
+    play() {
+        console.log("\nLet's play!");
 
-		this.board.initializeBoard();
+        this.board.initializeBoard();
 
-		this.board.displayBoard();
+        this.board.displayBoard();
 
-		this.playerTurn(this.currentPlayer);
-	}
+        this.takeTurn(this.currentPlayer);
+    }
 
-	playerTurn(currentPlayer) {
-		console.log(`\nIt's ${this.currentPlayer.name}'s turn.`);
-		const enteredCoordinate = readlineSync.question(
-			"\nPlease enter the coordinate for your move (ex: A1): "
-		);
-		const coordinate = new Coordinate(enteredCoordinate);
-		console.log(coordinate);
+    takeTurn(currentPlayer) {
+        console.log(`\nIt's ${this.currentPlayer.name}'s turn.`);
+        const enteredCoordinate = readlineSync.question(
+            "\nPlease enter the coordinate for your move (ex: A1): "
+        );
+        const coordinate = new Coordinate(enteredCoordinate);
+        console.log(coordinate);
 
-		if (coordinate) {
-			const move = new Move(currentPlayer, coordinate);
-			this.board.updateBoard(move);
-		}
-	}
+        if (coordinate) {
+            const move = new Move(currentPlayer, coordinate);
+            this.board.updateBoard(move);
+        }
+    }
 }
