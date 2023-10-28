2afe504964f88da90bfc50241714e8bb93070ab5
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-10-28 09:39:11 -0500

    Moved coordinate transformation logic into coordinate constructor.

--- coordinate.js
+++ coordinate.js
@@ -1,37 +1,63 @@
+import { transform } from "typescript";
+
 export class Coordinate {
-	constructor(coordinate) {
-		if (coordinate.length !== 2) {
-			throw new Error("Invalid coordinate: length should be 2.");
-		}
+    constructor(coordinate) {
+        if (coordinate.length !== 2) {
+            throw new Error("Invalid coordinate: length should be 2.");
+        }
 
-		const [x, y] = coordinate.split("");
-		this.validateCoordinate(x, y);
+        const [x, y] = coordinate.split("");
+        this.validateCoordinate(x, y);
 
-		this.x = x;
-		this.y = y;
-	}
+        this.x = this.transformXCoordinateIntoIndex(x);
+        this.y = this.transformYCoordinateIntoIndex(y);
+    }
 
-	validateCoordinate(x, y) {
-		if (!this.isXValid(x)) {
-			throw new Error("Invalid coordinate: x value invalid.");
-		}
+    validateCoordinate(x, y) {
+        if (!this.isXValid(x)) {
+            throw new Error("Invalid coordinate: x value invalid.");
+        }
 
-		if (!this.isYValid(y)) {
-			throw new Error("Invalid coordinate: y value invalid.");
-		}
-	}
+        if (!this.isYValid(y)) {
+            throw new Error("Invalid coordinate: y value invalid.");
+        }
+    }
 
-	isXValid(x) {
-		if (x == "A" || x == "B" || x == "C") {
-			return true;
-		}
-		return false;
-	}
+    isXValid(x) {
+        if (x == "A" || x == "B" || x == "C") {
+            return true;
+        }
+        return false;
+    }
 
-	isYValid(y) {
-		if (y == "1" || y == "2" || y == "3") {
-			return true;
-		}
-		return false;
-	}
+    isYValid(y) {
+        if (y == "1" || y == "2" || y == "3") {
+            return true;
+        }
+        return false;
+    }
+
+    transformXCoordinateIntoIndex(coordinate) {
+        if (coordinate == "A") {
+            return 0;
+        } else if (coordinate == "B") {
+            return 1;
+        } else if (coordinate == "C") {
+            return 2;
+        } else {
+            throw new Error("Invalid coordinate: x value invalid.");
+        }
+    }
+
+    transformYCoordinateIntoIndex(coordinate) {
+        if (coordinate == "1") {
+            return 0;
+        } else if (coordinate == "2") {
+            return 1;
+        } else if (coordinate == "3") {
+            return 2;
+        } else {
+            throw new Error("Invalid coordinate: y value invalid.");
+        }
+    }
 }
--- move.js
+++ move.js
@@ -1,30 +1,6 @@
 export class Move {
-	constructor(player, coordinate) {
-		this.player = player;
-		this.coordinate = coordinate;
-	}
-
-	transformXCoordinateIntoIndex(coordinate) {
-		if (coordinate == "A") {
-			return 0;
-		} else if (coordinate == "B") {
-			return 1;
-		} else if (coordinate == "C") {
-			return 2;
-		} else {
-			throw new Error("Invalid coordinate: x value invalid.");
-		}
-	}
-
-	transformYCoordinateIntoIndex(coordinate) {
-		if (coordinate == "1") {
-			return 0;
-		} else if (coordinate == "2") {
-			return 1;
-		} else if (coordinate == "3") {
-			return 2;
-		} else {
-			throw new Error("Invalid coordinate: y value invalid.");
-		}
-	}
+    constructor(player, coordinate) {
+        this.player = player;
+        this.coordinate = coordinate;
+    }
 }
