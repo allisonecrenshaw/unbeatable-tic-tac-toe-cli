a505825628995b37bee99d6c0e1551ec164fdd84
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-10-30 14:49:30 -0500

    Added switchPlayer and adjusted function logic.

--- game.js
+++ game.js
@@ -4,37 +4,42 @@ import { Move } from "./move.js";
 import * as readlineSync from "readline-sync";
 
 export class Game {
-    constructor(gameMode, player1, player2) {
-        this.board = new Board();
-        this.player1 = player1;
-        this.player2 = player2;
-        this.currentPlayer = player1;
-        this.gameMode = gameMode;
-        this.gameWon = false;
-        this.winner = null;
-    }
+	constructor(gameMode, player1, player2) {
+		this.board = new Board();
+		this.player1 = player1;
+		this.player2 = player2;
+		this.currentPlayer = player1;
+		this.gameMode = gameMode;
+		this.gameWon = false;
+		this.winner = null;
+	}
 
-    play() {
-        console.log("\nLet's play!");
+	play() {
+		console.log("\nLet's play!");
 
-        this.board.initializeBoard();
+		this.board.initializeBoard();
 
-        this.board.displayBoard();
+		while (this.gameWon === false) {
+			this.board.displayBoard;
+			this.takeTurn();
+			this.currentPlayer = this.switchPlayer();
+		}
+	}
 
-        this.takeTurn(this.currentPlayer);
-    }
+	takeTurn() {
+		console.log(`\nIt's ${this.currentPlayer.name}'s turn.`);
+		const enteredCoordinate = readlineSync.question(
+			"\nPlease enter the coordinate for your move (ex: A1): "
+		);
+		const coordinate = new Coordinate(enteredCoordinate);
 
-    takeTurn(currentPlayer) {
-        console.log(`\nIt's ${this.currentPlayer.name}'s turn.`);
-        const enteredCoordinate = readlineSync.question(
-            "\nPlease enter the coordinate for your move (ex: A1): "
-        );
-        const coordinate = new Coordinate(enteredCoordinate);
-        console.log(coordinate);
+		if (coordinate) {
+			const move = new Move(currentPlayer, coordinate);
+			this.board.updateBoard(move);
+		}
+	}
 
-        if (coordinate) {
-            const move = new Move(currentPlayer, coordinate);
-            this.board.updateBoard(move);
-        }
-    }
+	switchPlayer() {
+		return this.currentPlayer === this.player1 ? this.player2 : this.player1;
+	}
 }
