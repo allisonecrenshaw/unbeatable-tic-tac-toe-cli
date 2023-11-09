9fb49d414b24c04b34c72720a4a29131965d7983
Allison Crenshaw <allisonecrenshaw@gmail.com>
2023-11-08 21:42:01 -0600

    Initiating home component file.

---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>Welcome to My Vue App</h1>
+		<!-- Other content for your home page -->
+	</div>
+</template>
+
+<script>
+export default {
+	// Your component logic goes here
+};
+</script>
+
+<style scoped>
+/* Your component-specific styles go here */
+</style>
