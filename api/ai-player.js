edf866aef8308eec5f899332d4e919cbf883f933
allisonecrenshaw <allisonecrenshaw@gmail.com>
2023-11-29 23:07:10 -0700

    Renamed file ai to ai-player.

--- api/ai-player.js
+++ api/ai-player.js
@@ -1,6 +1,6 @@
 import { Player } from "./player";
 
-export class AI extends Player{
+export class AIPlayer extends Player{
     takeTurn() {
         console.log(`\nIt's the computer's turn.`);
     }
