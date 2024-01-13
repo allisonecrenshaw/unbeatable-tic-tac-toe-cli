5681992a65a55911808c7f9258ce201186b86c5c
allisonecrenshaw <allisonecrenshaw@gmail.com>
2024-01-12 23:11:40 -0600

    Removing validation logic from coordinate class. Will put this logic elsewhere.

--- api/coordinate.js
+++ api/coordinate.js
@@ -1,63 +1,30 @@
 export class Coordinate {
-    constructor(coordinate) {
-        if (coordinate.length !== 2) {
-            throw new Error(
-                "Invalid coordinate: coordinate should only have two characters (ex: A1)"
-            );
-        }
+  constructor(coordinate) {
+    this.x = this.transformXCoordinateIntoIndex(x);
+    this.y = this.transformYCoordinateIntoIndex(y);
+  }
 
-        const [x, y] = coordinate.split("");
-        this.validateCoordinate(x, y);
-
-        this.x = this.transformXCoordinateIntoIndex(x);
-        this.y = this.transformYCoordinateIntoIndex(y);
+  transformXCoordinateIntoIndex(coordinate) {
+    if (coordinate == 'A') {
+      return 0;
+    } else if (coordinate == 'B') {
+      return 1;
+    } else if (coordinate == 'C') {
+      return 2;
+    } else {
+      throw new Error('Invalid coordinate: x value invalid.');
     }
+  }
 
-    validateCoordinate(x, y) {
-        if (!this.isXValid(x)) {
-            throw new Error("Invalid coordinate: x value invalid.");
-        }
-
-        if (!this.isYValid(y)) {
-            throw new Error("Invalid coordinate: y value invalid.");
-        }
+  transformYCoordinateIntoIndex(coordinate) {
+    if (coordinate == '1') {
+      return 0;
+    } else if (coordinate == '2') {
+      return 1;
+    } else if (coordinate == '3') {
+      return 2;
+    } else {
+      throw new Error('Invalid coordinate: y value invalid.');
     }
-
-    isXValid(x) {
-        if (x == "A" || x == "B" || x == "C") {
-            return true;
-        }
-        return false;
-    }
-
-    isYValid(y) {
-        if (y == "1" || y == "2" || y == "3") {
-            return true;
-        }
-        return false;
-    }
-
-    transformXCoordinateIntoIndex(coordinate) {
-        if (coordinate == "A") {
-            return 0;
-        } else if (coordinate == "B") {
-            return 1;
-        } else if (coordinate == "C") {
-            return 2;
-        } else {
-            throw new Error("Invalid coordinate: x value invalid.");
-        }
-    }
-
-    transformYCoordinateIntoIndex(coordinate) {
-        if (coordinate == "1") {
-            return 0;
-        } else if (coordinate == "2") {
-            return 1;
-        } else if (coordinate == "3") {
-            return 2;
-        } else {
-            throw new Error("Invalid coordinate: y value invalid.");
-        }
-    }
+  }
 }
