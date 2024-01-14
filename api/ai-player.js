ae8481c37d49a664b35e52096d3ed0d8ed501473
Allison Crenshaw <allisonecrenshaw@gmail.com>
2024-01-13 22:47:08 -0600

    Stubbing methods for ai player.

--- api/ai-player.js
+++ api/ai-player.js
@@ -1,7 +1,14 @@
-import { Player } from "./player";
+import { Player } from './player';
 
-export class AIPlayer extends Player{
-    takeTurn() {
-        console.log(`\nIt's the computer's turn.`);
+export class AIPlayer extends Player {
+  takeTurn() {
+    console.log(`\nIt's the computer's turn.`);
+  }
+
+  getUnoccupiedCellCoordinates(board) {
+    console.log(`Getting available coordinates.`);
+    for (let rowIndex = 0; rowIndex <= 3; rowIndex++) {
+      console.log(`Check each row.`);
     }
+  }
 }
