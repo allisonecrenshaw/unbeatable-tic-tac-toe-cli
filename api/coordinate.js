4c9b85a01a995c085412c411a8c99b2fe1cbc039
allisonecrenshaw <allisonecrenshaw@gmail.com>
2024-01-14 16:48:34 -0600

    Restoring previous version of coordinate.

--- api/coordinate.js
+++ api/coordinate.js
@@ -1,7 +1,40 @@
 export class Coordinate {
   constructor(coordinate) {
+    if (coordinate.length !== 2) {
+      throw new Error(
+        'Invalid coordinate: coordinate should only have two characters (ex: A1)',
+      );
+    }
+
+    const [x, y] = coordinate.split('');
+    this.validateCoordinate(x, y);
+
     this.x = this.transformXCoordinateIntoIndex(x);
     this.y = this.transformYCoordinateIntoIndex(y);
+  }
+
+  validateCoordinate(x, y) {
+    if (!this.isXValid(x)) {
+      throw new Error('Invalid coordinate: x value invalid.');
+    }
+
+    if (!this.isYValid(y)) {
+      throw new Error('Invalid coordinate: y value invalid.');
+    }
+  }
+
+  isXValid(x) {
+    if (x == 'A' || x == 'B' || x == 'C') {
+      return true;
+    }
+    return false;
+  }
+
+  isYValid(y) {
+    if (y == '1' || y == '2' || y == '3') {
+      return true;
+    }
+    return false;
   }
 
   transformXCoordinateIntoIndex(coordinate) {
