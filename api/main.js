e5605da89bc2a71170698dbc48a17cc5e5daf454
allisonecrenshaw <allisonecrenshaw@gmail.com>
2024-01-15 00:17:37 -0600

    Adjusted player creation for AI player.

--- api/main.js
+++ api/main.js
@@ -1,5 +1,7 @@
 import { Game } from './game.js';
 import * as utilities from './utilities.js';
+import * as constants from './constants.js';
+import { AIPlayer } from './ai-player.js';
 
 utilities.displayWelcomeMessage();
 
@@ -8,7 +10,12 @@ utilities.displayGameModeOptions();
 let gameMode = utilities.getGameMode();
 
 let player1 = utilities.createPlayer1();
-let player2 = utilities.createPlayer2(gameMode, player1.name);
+
+if (gameMode === constants.COMPUTER_MODE) {
+  let player2 = new AIPlayer();
+} else {
+  let player2 = utilities.createPlayer2(gameMode, player1.name);
+}
 
 const game = new Game(gameMode, player1, player2);
 
