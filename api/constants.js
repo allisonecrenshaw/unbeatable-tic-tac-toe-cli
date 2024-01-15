90a108ded697f1279281c014b617814a482f1863
allisonecrenshaw <allisonecrenshaw@gmail.com>
2024-01-14 19:46:36 -0600

    Created consts file.

---
+++ api/constants.js
@@ -0,0 +1,7 @@
+// Game Modes
+export const COMPUTER_MODE = 'computer';
+export const FRIEND_MODE = 'friend';
+export const DEFAULT_GAME_MODE = FRIEND_MODE;
+
+// Other
+export const MAX_INPUT_ATTEMPTS = 3;
