560f44506b4221b9c9a052cfa9657aee8fc24cb6
allisonecrenshaw <allisonecrenshaw@gmail.com>
2024-01-15 00:33:34 -0600

    Adding row and column nums.

--- api/constants.js
+++ api/constants.js
@@ -3,5 +3,9 @@ export const COMPUTER_MODE = 'computer';
 export const FRIEND_MODE = 'friend';
 export const DEFAULT_GAME_MODE = FRIEND_MODE;
 
+// Board Constants
+export const numOfRows = 3;
+export const numOfColumns = 3;
+
 // Other
 export const MAX_INPUT_ATTEMPTS = 3;
