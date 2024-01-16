d3b5c576fffff81f67ce8c14e5900db8b1261e12
allisonecrenshaw <allisonecrenshaw@gmail.com>
2024-01-15 22:00:24 -0600

    Fixed constructor to include super.

--- api/simulated-game.js
+++ api/simulated-game.js
@@ -2,11 +2,10 @@ import { Game } from './game.js';
 
 export class SimulatedGame extends Game {
   constructor(game) {
+    super(game.gameMode, game.player1, game.player2);
+
     this.board = game.board;
-    this.player1 = game.player1;
-    this.player2 = game.player2;
     this.currentPlayer = game.currentPlayer;
-    this.playerMode = playerMode;
     this.finished = false;
     this.winningPlayer = null;
     this.executedMoves = [];
