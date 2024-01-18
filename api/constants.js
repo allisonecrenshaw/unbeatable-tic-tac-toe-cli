20f66db377a0ce2c56da3ff5c4bbca7c11d48783
allisonecrenshaw <allisonecrenshaw@gmail.com>
2024-01-17 22:22:58 -0600

    Adding center coordinate as a constant.

--- api/constants.js
+++ api/constants.js
@@ -1,3 +1,5 @@
+import { Coordinate } from './coordinate.js';
+
 // Game Modes
 export const COMPUTER_MODE = 'computer';
 export const FRIEND_MODE = 'friend';
@@ -6,6 +8,9 @@ export const DEFAULT_GAME_MODE = COMPUTER_MODE;
 // Board Constants
 export const NUM_OF_ROWS = 3;
 export const NUM_OF_COLUMNS = 3;
+
+// Coordinate Constants
+export const CENTER_COORDINATE = new Coordinate('index', [1, 1]);
 
 // Other
 export const MAX_INPUT_ATTEMPTS = 3;
